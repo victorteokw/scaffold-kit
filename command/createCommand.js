2a48135960e40b99f0da291f2d1c47f164230986
Zhang Kaiyu <yeannylam@gmail.com>
2018-10-26 14:42:27 +0800

    align code

--- command/createCommand.js
+++ command/createCommand.js
@@ -10,30 +10,30 @@ const error = require('../error');
  * @property {Boolean} saveToPreference - Thether save the option to .rc file.
  */
 
- /**
-  * The execution information object.
-  *
-  * @typedef {Object} ExecutionInformation
-  * @property {String[]} args - The command line arguments.
-  * @property {Object} options - The final execution options.
-  * @property {String} projDir - The original project root directory.
-  */
+/**
+ * The execution information object.
+ *
+ * @typedef {Object} ExecutionInformation
+ * @property {String[]} args - The command line arguments.
+ * @property {Object} options - The final execution options.
+ * @property {String} projDir - The original project root directory.
+ */
 
- /**
-  * The function that relocates project root directory.
-  *
-  * @callback RelocateProjDir
-  * @param {ExecutionInformation} info - The execution information.
-  * @return {String} The project directory that relocated.
-  */
+/**
+ * The function that relocates project root directory.
+ *
+ * @callback RelocateProjDir
+ * @param {ExecutionInformation} info - The execution information.
+ * @return {String} The project directory that relocated.
+ */
 
-  /**
-   * The function that executes the command.
-   *
-   * @callback CommandExecution
-   * @param {ExecutionInformation} info - The execution information.
-   * @return {Void} This function returns nothing.
-   */
+/**
+ * The function that executes the command.
+ *
+ * @callback CommandExecution
+ * @param {ExecutionInformation} info - The execution information.
+ * @return {Void} This function returns nothing.
+ */
 
 /**
  * Create an executable command.
