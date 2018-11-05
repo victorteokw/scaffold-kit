df695f2f806fc95703f7d83fb04d5538b378a0af
Zhang Kaiyu <yeannylam@gmail.com>
2018-11-05 15:27:51 +0800

    async execution

--- executor/executeAllInstructions.js
+++ executor/executeAllInstructions.js
@@ -1,5 +1,6 @@
 const assign = require('lodash/assign');
 const each = require('lodash/each');
+const eachAsync = require('series-async-each');
 const sortBy = require('lodash/sortBy');
 const keys = require('lodash/keys');
 const instRegistry = require('./instRegistry');
@@ -8,22 +9,26 @@ const { getExecutorOptions } = require('./executorOptions');
 const outputMessage = require('./outputMessage');
 const executions = require('./executions');
 
-const executeAllInstructions = () => {
+const executeAllInstructions = async () => {
   const executable = rawInstsToExecutable(instRegistry.getAll());
   const { files, dependencies, commands, directories } = executable;
-  each(sortBy(keys(files)), (insts) => {
-    each(insts, ({ name, params }) => {
-      executions[name](assign(getExecutorOptions(), params));
+  await eachAsync(sortBy(keys(files)), async (insts) => {
+    await eachAsync(insts, async ({ name, params }) => {
+      const executionParams = assign(getExecutorOptions(), params);
+      const messages = await executions[name](executionParams);
+      each(messages, (message) => {
+        outputMessage(...message);
+      });
     });
   });
-  each(sortBy(keys(directories)), ({ name, params }) => {
-    executions[name](assign(getExecutorOptions(), params));
+  await eachAsync(sortBy(keys(directories)), async ({ name, params }) => {
+    await executions[name](assign(getExecutorOptions(), params));
   });
-  each(sortBy(keys(dependencies)), ({ name, params }) => {
-    executions[name](assign(getExecutorOptions(), params));
+  await eachAsync(sortBy(keys(dependencies)), async ({ name, params }) => {
+    await executions[name](assign(getExecutorOptions(), params));
   });
-  each(commands, ({ name, params }) => {
-    executions[name](assign(getExecutorOptions(), params));
+  await eachAsync(commands, async ({ name, params }) => {
+    await executions[name](assign(getExecutorOptions(), params));
   });
 };
 
