c0096f91d5cbe519c4e2976de3925cd232747585
Zhang Kaiyu <yeannylam@gmail.com>
2019-03-01 23:19:00 +0800

    remove wrong doc

--- command/executeCommand.js
+++ command/executeCommand.js
@@ -1,15 +1,6 @@
 const eachAsync = require('series-async-each');
-const error = require('../error');
 const loadCommand = require('./loadCommand');
 
-/**
- * Execute a command.
- *
- * @param {Application} app - The scaffold app.
- * @param {Command} command - The command to be executed.
- * @param {ExecutionInformation} executionParams - The execution parameters.
- * @return {Void} This function returns nothing.
- */
 const executeCommand = async (app, command, executionParams) => {
 
   if (command.composedOf) {
