408f38fa5911fbf117801123e17c76997f575f33
yangxiaodong <yangxiaodong@qianmi.com>
2019-05-25 16:57:48 +0800

    recovery: recovery createFile

--- src/handlers/rollbackJSONFile.ts
+++ src/handlers/rollbackJSONFile.ts
@@ -1,21 +1,30 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import { isEqual, cloneDeep } from 'lodash';
+import RollbackJSONFileInfo from '../instructions/RollbackJSONFileInfo';
+import Reporter from '../Reporter';
+import isDefined from '../utilities/isDefined';
 
-const rollbackJSONFile = ({ at, rollbacker, silent }) => {
+const rollbackJSONFile = ( params:RollbackJSONFileInfo,reporter: Reporter) => {
+  let { at, rollbacker } = params;
+  if (!isDefined(at) || !isDefined(rollbacker) ) {
+    throw new Error(
+      `You should provide at and rollbacker.`,
+    );
+  }
   const dest = at;
   if (fs.existsSync(dest)) {
     const before = JSON.parse(fs.readFileSync(dest).toString());
     const after = rollbacker(cloneDeep(before));
     if (isEqual(before, after)) {
-      return ['unchanged', 'yellow', at, silent];
+      reporter.push({ message: 'unchanged', file: at });
     } else {
       fs.writeFileSync(at, JSON.stringify(after, null, 2) + '\n');
-      return ['rollback', 'green', at, silent];
+      reporter.push({ message: 'rollback', file: at });
     }
   } else {
     // file not exist, can not rollback
-    return ['not exist', 'red', at, silent];
+    reporter.push({ message: 'not exist', file: at });
   }
 };
 
