bc3eae531eb385dcdb2ab86dd34877def36753b4
Zhang Kaiyu <yeannylam@gmail.com>
2019-07-22 14:28:53 +0800

    fixing redirecting working directory test in win32

--- tests/middlewares/redirectWorkingDirectory.ts
+++ tests/middlewares/redirectWorkingDirectory.ts
@@ -1,3 +1,4 @@
+import * as os from 'os';
 import Context from '../../src/Context';
 import redirectWorkingDirectory from '../../src/middlewares/redirectWorkingDirectory';
 import nullExecutable from '../../src/nullExecutable';
@@ -6,18 +7,18 @@ describe('Redirect working directory', () => {
 
   it("updates context's working directory", async () => {
     const ctx = new Context({
-      wd: '/home/clock-man',
-      args: ['../weather-maker-man'],
+      wd: os.platform() === 'win32' ? 'C:\\Users\\Clock Man' : '/home/clock-man',
+      args: [os.platform() === 'win32' ? '..\\Weather Maker Man' : '../weather-maker-man'],
       options: {}
     });
     await redirectWorkingDirectory(ctx, nullExecutable);
-    expect(ctx.wd).toBe('/home/weather-maker-man');
+    expect(ctx.wd).toBe(os.platform() === 'win32' ? 'C:\\Users\\Weather Maker Man' : '/home/weather-maker-man');
   });
 
   it('removes the first argument from context', async () => {
     const ctx = new Context({
-      wd: '/home/clock-man',
-      args: ['../weather-maker-man'],
+      wd: os.platform() === 'win32' ? 'C:\\Users\\Clock Man' : '/home/clock-man',
+      args: ['..\\Weather Maker Man'],
       options: {}
     });
     await redirectWorkingDirectory(ctx, nullExecutable);
@@ -26,13 +27,13 @@ describe('Redirect working directory', () => {
 
   it('does nothing if no arguments in context', async () => {
     const ctx = new Context({
-      wd: '/home/clock-man',
+      wd: os.platform() === 'win32' ? 'C:\\Users\\Clock Man' : '/home/clock-man',
       args: [],
       options: {}
     });
     await redirectWorkingDirectory(ctx, nullExecutable);
     expect(ctx.args).toEqual([]);
-    expect(ctx.wd).toBe('/home/clock-man');
+    expect(ctx.wd).toBe(os.platform() === 'win32' ? 'C:\\Users\\Clock Man' : '/home/clock-man');
   });
 
 });
